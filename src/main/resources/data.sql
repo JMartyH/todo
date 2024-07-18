7aad1888dd48443a4d7e5a3b55f1323c5523bffc
Marty <jmjmjm373@gmail.com>
2024-07-18 20:32:20 +0800

    Added a data.sql for dummy data and annotation on main to remove page warn.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO todos (title, description, creation_date, due_date, status) VALUES
+('Buy groceries', 'Milk, eggs, bread, cheese', now(), '2024-12-15 18:00:00', 'PENDING'),
+('Finish project report', 'Finalize the analysis and write the conclusion', now(), '2024-12-20 17:00:00', 'PENDING'),
+('Meeting', 'Meet up w/', now(), '2024-07-20 17:00:00', 'CANCELLED'),
+('Book doctor appointment', 'Schedule a check-up for next week', now(), '2023-12-12 14:00:00', 'COMPLETED');
