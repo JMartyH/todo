88c32894042c869706bafdb6fb0388b444e3f8fb
Marty <jmjmjm373@gmail.com>
2024-07-26 16:53:10 +0800

    Removed unnecessary extends to PagingAndSortingRepository.

--- src/main/java/com/todo/repository/ToDoRepository.java
+++ src/main/java/com/todo/repository/ToDoRepository.java
@@ -5,14 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ToDoRepository extends JpaRepository<ToDoEntity, Long>, PagingAndSortingRepository<ToDoEntity, Long> {
+public interface ToDoRepository extends JpaRepository<ToDoEntity, Long>{
 
     Optional<ToDoEntity> getByTitle(String title);
 
